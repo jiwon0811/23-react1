e376b6b1e178d3137a05dd71864035c6c8967b87
jiwon0811 <shery0406@naver.com>
2023-03-30 16:09:26 +0900

    5주차

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Clock from './chapter_04/Clock';
 
-import Library from './chaper_03/Library';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+setInterval(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+  }, 1000);
+
+ 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

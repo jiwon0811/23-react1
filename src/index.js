d91aacee7f295ae677cc4a8b10a51f57d5d3ba84
jiwon0811 <shery0406@naver.com>
2023-04-27 16:26:09 +0900

    9주차 실습

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+import Library from './chaper_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import useCounter from './chapter_07/Accommodate';
 import Accommodate from './chapter_07/Accommodate';
-
+import ConfirmButton from './Chapter_08/Confirm Button';
+import LandingPage from './Chapter_09/LandingPage';
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <Accommodate/>
+      <LandingPage/>
     </React.StrictMode>
   );
   
